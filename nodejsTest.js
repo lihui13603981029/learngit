fdb42a0dfbabc10336d4020e0799cda8d325d5ea
lihui13603981029 <397727593@qq.com>
2017-06-17 17:46:38 +0800

    modify nodejs

--- nodejsTest.js
+++ nodejsTest.js
@@ -8,4 +8,4 @@ http.createServer(function(request, response) {
 }).listen(8000, "127.0.0.1");
 
 console.log("Creat server on http://127.0.0.1:8000/");
-console.log("我是一个伟大的程序员,但是我还得努力");
+console.log("我是一个伟大的程序员,但是我还得努力,加油");
